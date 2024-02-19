ba74b02fe7397a465cc65976a5464494364fa56c
glennwanjiru <glennnjoroge@gmail.com>
2024-02-19 20:21:17 +0300

    Start

---
+++ lib/themes/dark_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+        background: Colors.grey.shade900,
+        primary: Colors.grey.shade500,
+        secondary: Colors.grey.shade800,
+        inversePrimary: Colors.grey.shade300));
