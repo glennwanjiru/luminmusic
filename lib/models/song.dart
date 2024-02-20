f664c22ee96d478d48943c15fc39aacfeabb7019
glennwanjiru <glennnjoroge@gmail.com>
2024-02-20 20:55:52 +0300

    Playlist

--- lib/models/song.dart
+++ lib/models/song.dart
@@ -1,1 +1,13 @@
-class Song {}
+class Song {
+  final String songName;
+  final String artistName;
+  final String albumArtImagePath;
+  final String audioPath;
+
+  Song({
+    required this.songName,
+    required this.artistName,
+    required this.albumArtImagePath,
+    required this.audioPath,
+  });
+}
